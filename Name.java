c376b2d54abbe7672713841fe54b7f8cb4fa5e12
Yadvendra124 <23bit176iet.davv.edu>
2024-08-02 22:04:50 +0530

    Array of Name

---
+++ Name.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Name {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        String Name[] =new String[size];
+        for(int i=0;i<size;i++){
+            Name[i] = sc.next();
+        }
+        for(int i=0;i<Name.length;i++){
+           System.out.println(Name[i]);
+        }
+    }
+}
