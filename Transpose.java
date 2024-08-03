feef04e632724ed049ecbd0f61aa2e639c5e670c
Yadvendra124 <23bit176iet.davv.edu>
2024-08-03 09:07:24 +0530

    Transpose of matrix

---
+++ Transpose.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Transpose{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of row : ");
+        int row = sc.nextInt ();
+        System.out.println("Enter the no. of column : ");
+        int column =sc.nextInt();
+        int matrix[][] = new int[row][column];
+        for(int i=0;i<row;i++){
+            for(int j =0;j<column;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("The Transpose of matrix : ");
+        for(int j=0;j<column;j++){
+            for(int i=0;i<row;i++){
+              System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+            
+        }
+        ;
+        
+        
+    }
+
+}
