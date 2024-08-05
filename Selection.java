648387e518caa3c977bf341cfa0f5f60d6895bf1
Yadvendra124 <23bit176iet.davv.edu>
2024-08-05 23:34:26 +0530

    Selection sort

--- Selection.java
+++ Selection.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Selection {
     public static void printArray(int arr[]){
         for(int i=0;i<arr.length;i++){
-        System.out.print(arr[i]+"  ");
+        System.out.print(arr[i]+" ");
         }
     }
     
